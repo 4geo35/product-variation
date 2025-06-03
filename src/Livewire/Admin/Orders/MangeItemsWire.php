9959c7eb94273310357228fd3fe07d22c76af205
Labrock <vladimirpeshkov5@yandex.ru>
2025-06-03 17:41:27 +0300

    make change quantity

--- src/Livewire/Admin/Orders/MangeItemsWire.php
+++ src/Livewire/Admin/Orders/MangeItemsWire.php
@@ -2,7 +2,11 @@
 
 namespace GIS\ProductVariation\Livewire\Admin\Orders;
 
+use GIS\ProductVariation\Facades\OrderActions;
 use GIS\ProductVariation\Interfaces\OrderInterface;
+use GIS\ProductVariation\Interfaces\OrderItemInterface;
+use GIS\ProductVariation\Models\OrderItem;
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\View\View;
 use Livewire\Component;
 
@@ -10,10 +14,91 @@ class MangeItemsWire extends Component
 {
     public OrderInterface $order;
 
+    public int|null $itemId = null;
+    public int $quantity = 1;
+    public bool $displayQuantity = false;
+
+    public function updated($property, $value): void
+    {
+        if ($property === "quantity") {
+            if ($value <= 0) $this->quantity = 1;
+        }
+    }
+
     public function render(): View
     {
         $items = $this->order->items()->with("product", "variation")->get();
         debugbar()->info($items);
         return view('pv::livewire.admin.orders.mange-items-wire', compact('items'));
+    }
+
+    public function closeQuantity(): void
+    {
+        $this->resetFields();
+        $this->displayQuantity = false;
+    }
+
+    public function increaseQuantity(): void
+    {
+        $this->quantity++;
+    }
+
+    public function decreaseQuantity(): void
+    {
+        $this->quantity--;
+    }
+
+    public function showEditQuantity(int $itemId): void
+    {
+        $this->itemId = $itemId;
+        $item = $this->findModel();
+        if (! $item) { return; }
+        if (! $this->checkAuth("update")) { return; }
+
+        $this->quantity = $item->quantity;
+        $this->displayQuantity = true;
+    }
+
+    public function updateQuantity(): void
+    {
+        $item = $this->findModel();
+        if (! $item) { return; }
+        if (! $this->checkAuth("update")) { return; }
+
+        OrderActions::changeOrderItemQuantity($item, $this->quantity, false);
+        OrderActions::recalculateOrderTotal($this->order);
+        $this->order->fresh();
+        $this->dispatch("order-updated");
+        $this->closeQuantity();
+        session()->flash("order-items-success", "Позиция заказа успешно обновлена");
+    }
+
+    protected function resetFields(): void
+    {
+        $this->reset(["itemId", "quantity"]);
+    }
+
+    protected function findModel(): ?OrderItemInterface
+    {
+        $itemModelClass = config("product-variation.customOrderItemModel") ?? OrderItem::class;
+        $item = $itemModelClass::find($this->itemId);
+        if (! $item) {
+            session()->flash("order-items-error", "Позиция заказа не найдена");
+            $this->closeQuantity();
+            return null;
+        }
+        return $item;
+    }
+
+    protected function checkAuth(string $action): bool
+    {
+        try {
+            $this->authorize($action, $this->order);
+            return true;
+        } catch (AuthorizationException $e) {
+            session()->flash("order-customer-error", "Неавторизованное действие");
+            $this->closeQuantity();
+            return false;
+        }
     }
 }
--- src/resources/views/admin/order-items/includes/table.blade.php
+++ src/resources/views/admin/order-items/includes/table.blade.php
@@ -14,7 +14,7 @@
             <tr>
                 <td>
                     <div class="flex items-center justify-start space-x-2">
-                        <span class="font-semibold">{{ $item->title }}</span>
+                        <span class="font-semibold text-nowrap">{{ $item->title }}</span>
                         @if ($item->product)
                             <a href="{{ route("admin.products.show", ["product" => $item->product]) }}"
                                target="_blank"
@@ -24,10 +24,44 @@
                         @endif
                     </div>
                 </td>
-                <td>{{ $item->sku }}</td>
-                <td>{{ $item->human_price }} руб.</td>
-                <td>{{ $item->quantity }}</td>
-                <td>{{ $item->human_total }} руб.</td>
+                <td><span class="text-nowrap">{{ $item->sku }}</span></td>
+                <td><span class="text-nowrap">{{ $item->human_price }} руб.</span></td>
+                <td>
+                    @if ($displayQuantity && $itemId === $item->id)
+                        <div class="flex items-center space-x-indent-half">
+                            <div class="flex items-center justify-start">
+                                <button type="button" class="btn btn-outline-secondary btn-sm rounded-e-none border-e-0"
+                                        wire:click="decreaseQuantity"
+                                        @if ($quantity <= 1) disabled @endif>
+                                    <x-vc::ico.minus />
+                                </button>
+                                <input type="number" aria-label="Количество"
+                                       class="form-control form-control-sm border-secondary text-center rounded-none max-w-24 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
+                                       wire:model.blur="quantity" min="1">
+                                <button type="button" class="btn btn-outline-secondary btn-sm rounded-s-none border-s-0"
+                                        wire:click="increaseQuantity">
+                                    <x-vc::ico.plus />
+                                </button>
+                            </div>
+                            <div class="flex items-center">
+                                <button type="button" class="btn btn-success btn-sm rounded-e-none"
+                                        wire:click="updateQuantity">
+                                    OK
+                                </button>
+                                <button type="button" class="btn btn-dark btn-sm rounded-s-none"
+                                        wire:click="closeQuantity">
+                                    <x-tt::ico.cross />
+                                </button>
+                            </div>
+                        </div>
+                    @else
+                        <button type="button" wire:click="showEditQuantity({{ $item->id }})"
+                                class="cursor-pointer text-primary hover:text-primary-hover">
+                            {{ $item->quantity }}
+                        </button>
+                    @endif
+                </td>
+                <td><span class="text-nowrap">{{ $item->human_total }} руб.</span></td>
                 <td></td>
             </tr>
         @endforeach
