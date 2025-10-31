36fcb3d530f246ba4d5924e7fc3936b859127a9e
Labrock <vladimirpeshkov5@yandex.ru>
2025-10-31 15:42:43 +0300

    render unit ont show page

--- src/resources/views/livewire/web/catalog/choose-variation-wire.blade.php
+++ src/resources/views/livewire/web/catalog/choose-variation-wire.blade.php
@@ -1,7 +1,9 @@
 <div>
     @if ($variation)
         <div class="flex flex-wrap items-center space-x-indent mb-indent">
-            <div class="text-4xl sm:text-5xl font-bold text-nowrap mb-2">{{ $variation->human_price }} р.</div>
+            <div class="text-4xl sm:text-5xl font-bold text-nowrap mb-2">
+                {{ $variation->human_price }} р. <span class="text-body/60 text-xl sm:text-2xl">/ {{ $variation->unit_text }}</span>
+            </div>
             @if ($variation->sale)
                 <div class="text-2xl sm:text-3xl font-medium text-body/60 line-through text-nowrap mb-2">{{ $variation->human_old_price }} р.</div>
             @endif
@@ -16,7 +18,9 @@
                     <label class="ml-indent-half flex flex-col items-start justify-start" for="productVariation-{{ $item->sku }}">
                         <span>{{ $item->title }}</span>
                         <span class="flex items-center justify-start space-x-indent-half">
-                            <span class="text-lg font-medium">{{ $item->human_price }} р.</span>
+                            <span class="text-lg font-medium">
+                                {{ $item->human_price }} р. <span class="text-body/60 text-sm">/ {{ $variation->unit_text }}</span>
+                            </span>
                             @if ($item->sale)
                                 <span class="text-sm font-medium text-body/60 line-through">{{ $item->human_old_price }} р.</span>
                             @endif
